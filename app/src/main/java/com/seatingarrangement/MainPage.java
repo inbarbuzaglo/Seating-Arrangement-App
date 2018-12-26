47e1be7beb40c4a25fbdf7c666f273ae1a8e0276
nirav <niraviad.nini@gmail.com>
2018-12-26 23:18:31 +0200

    add Admin SDK

--- app/src/main/java/com/seatingarrangement/MainPage.java
+++ app/src/main/java/com/seatingarrangement/MainPage.java
@@ -6,12 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        FileInputStream serviceAccount =
+                null;
+        try {
+            serviceAccount = new FileInputStream("file:///android_asset/seatingarrangement-fbdde-firebase-adminsdk-1wtj6-a65ae82bf1.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseOptions options = null;
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://seatingarrangement-fbdde.firebaseio.com")
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseApp.initializeApp(options);
 
         final Button goToLogin = (Button) findViewById(R.id.goToLogin);
         goToLogin.setOnClickListener(new View.OnClickListener() {
