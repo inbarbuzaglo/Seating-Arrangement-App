4bde61f83865a126bf88a7cc8f62a32850e8671b
nirav <niraviad.nini@gmail.com>
2018-12-28 13:17:30 +0200

    login and register with permmision, build super admin page, create event.....

--- app/src/main/java/com/seatingarrangement/MainPage.java
+++ app/src/main/java/com/seatingarrangement/MainPage.java
@@ -6,12 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class MainPage extends AppCompatActivity {
 
@@ -20,24 +14,6 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-//        FileInputStream serviceAccount = null;
-//        try {
-//            serviceAccount = new FileInputStream("file:///android_asset/seatingarrangement-fbdde-firebase-adminsdk-1wtj6-a65ae82bf1.json");
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        FirebaseOptions options = null;
-//        try {
-//            options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setDatabaseUrl("https://seatingarrangement-fbdde.firebaseio.com")
-//                    .build();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        FirebaseApp.initializeApp(options);
 
         final Button goToLogin = (Button) findViewById(R.id.goToLogin);
         goToLogin.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/seatingarrangement/SuperAdminPage.java
+++ app/src/main/java/com/seatingarrangement/SuperAdminPage.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,6 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SuperAdminPage extends AppCompatActivity {
@@ -29,6 +31,7 @@ public class SuperAdminPage extends AppCompatActivity {
 
     private ArrayList<String> mArrayList;
     private ArrayAdapter<String> mAdapter;
+    private Map<String, String> mEventIdToName;
 
     private FirebaseAuth mAuth;
 //    private FirebaseDatabase mFirebaseDatabase;
@@ -42,10 +45,37 @@ public class SuperAdminPage extends AppCompatActivity {
         mRef = FirebaseDatabase.getInstance().getReference();
         mArrayList = new ArrayList<>();
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mArrayList);
+        mEventIdToName = new HashMap<>();
 
         newEvent = findViewById(R.id.newEventBtn);
         mListView = findViewById(R.id.eventsListView);
         mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+                // Then you start a new Activity via Intent
+                Intent i = new Intent();
+                Log.d("~~~" , "d " + mEventIdToName);
+                Log.d("~~~" , "d " + mEventIdToName.keySet());
+                String idKey = null;
+                for(String key : mEventIdToName.keySet()){
+                    if(mEventIdToName.get(key).equals(mArrayList.get(position))){
+                        idKey = key;
+                        break;
+                    }
+                }
+                if(idKey != null){
+                    i.putExtra("key", idKey);
+                    mEventIdToName.remove(idKey);
+                    i.setClass(SuperAdminPage.this, Event.class);
+                    i.putExtra("position", position);
+                    i.putExtra("id", id);
+    //                finish();
+                    SuperAdminPage.this.startActivity(i);
+                }
+            }
+        });
 
 
         mRef.child("Event").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -55,10 +85,12 @@ public class SuperAdminPage extends AppCompatActivity {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     EventInfo e = snapshot.getValue(EventInfo.class);
                     System.out.println(e.getmName());
+                    mEventIdToName.put(snapshot.getKey(), e.getmName());
                     events.add(e);
                 }
                 for(EventInfo ev : events){
                     mArrayList.add(ev.getmName());
+
                     Log.d("~~~", "ev.getmName"+ev.getmName());
                 }
             }
@@ -71,13 +103,13 @@ public class SuperAdminPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(SuperAdminPage.this, CreateEvent.class);
-                finish();
+//                finish();
                 SuperAdminPage.this.startActivity(i);
             }
         });
     }
 
-    private void toastMessage(String message){
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+//    private void toastMessage(String message){
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//    }
 }
